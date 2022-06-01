74f15a20c43d582bedc29577eaea06733bfca97e
Chad Heidesch <chadheidesch@gmail.com>
2022-06-01 17:51:12 -0500

    start of homepage

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
-
+import React from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go'; 
+import { Stacked, Pie, Button, SparkLine } from '../components';
+import { earningData, SparklingAreaData, ecomPieChartDat } from '../contexts/ContextProvider';
+ 
 const Ecommerce = () => {
   return (
-    <div>Ecommerce</div>
+    <div className="mt-12">
+      <div className="flex flex-wrap lg:flex-no-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="font-bold text-gray-400">Earnings</p>
+              <p className="text-2xl">$87,375.29</p>
+            </div>
+          </div>
+          <div className="mt-6">
+            <Button 
+              color="white"
+              bgColor="blue"
+              text="Download"
+              borderRadius="10px"
+              size="md"
+            />
+          </div>
+        </div>
+
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg"
+            >
+
+            </div>
+          ))}
+
+        </div>
+      </div>
+    </div>
   )
 }
 
