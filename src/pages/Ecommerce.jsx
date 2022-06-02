1df5ceb191b3cc684663a573995b5845d70fe028
Chad Heidesch <chadheidesch@gmail.com>
2022-06-02 16:23:04 -0500

    ecommerce page

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { BsCurrencyDollar } from 'react-icons/bs';
 import { GoPrimitiveDot } from 'react-icons/go'; 
 import { Stacked, Pie, Button, SparkLine } from '../components';
-import { earningData, SparklingAreaData, ecomPieChartDat } from '../data/dummy';
+import { earningData, SparklingAreaData, ecomPieChartDat, SparklineAreaData } from '../data/dummy';
  
 const Ecommerce = () => {
   return (
     <div className="mt-12">
-      <div className="flex flex-wrap lg:flex-no-wrap justify-center">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
@@ -71,7 +71,52 @@ const Ecommerce = () => {
                   </p>
                 </div>
               </div>
+              <div className="mt-10 flex gap-10 flex-wrap justify-center">
+                <div className="border-r-1 border-color m-4 pr-10">
+                  <div>
+                    <p>
+                      <span className="text-3xl font-semibold">84,597</span>
+                      <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">35%</span>
+                    </p>
+                    <p className="text-gray-500 mt-1">Budget</p>
+                  </div>
+                  
+                  <div className="mt-8">
+                    <p>
+                      <span className="text-3xl font-semibold">39,541</span>
+                      
+                    </p>
+                    <p className="text-gray-500 mt-1">Expense</p>
+                  </div>
 
+                  <div className="mt-5">
+                    <SparkLine
+                      currentColor="blue"
+                      id="line-sparkLine"
+                      type="Line"
+                      height="80px"
+                      width="250px"
+                      data={SparklineAreaData}
+                      color="blue"
+                    />
+                  </div>
+
+                  <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+                </div>
+                  
+
+                </div>
+                <div>
+              <Stacked  width="320px" height="360px" />
+            </div>
+
+              </div>
             </div>
       </div>
     </div>
